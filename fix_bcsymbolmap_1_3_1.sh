25274a0c4d84abaee14e569649d225b74b595c2d
Mingway <mingwei.shi@hotmail.com>
2017-11-02 05:35:11 -0500

    remove sudo

--- fix_bcsymbolmap_1_3_1.sh
+++ fix_bcsymbolmap_1_3_1.sh
@@ -3,4 +3,4 @@
 poddir=$(echo `gem env gemdir`)
 embed_frameworks_script_file_path="${poddir}/gems/cocoapods-1.3.1/lib/cocoapods/generator/embed_frameworks_script.rb"
 echo "copying embed_frameworks_script.rb to ${embed_frameworks_script_file_path}"
-sudo curl -fsSL https://raw.githubusercontent.com/mingway1991/PodFramework/master/embed_frameworks_script.rb -o "$embed_frameworks_script_file_path"
+curl -fsSL https://raw.githubusercontent.com/mingway1991/PodFramework/master/embed_frameworks_script.rb -o "$embed_frameworks_script_file_path"
