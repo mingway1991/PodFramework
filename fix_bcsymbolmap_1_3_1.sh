1d21c99c2032a88e7e7b0bc6a1594e3a51314fa6
shimingwei <shimingwei@lvmama.com>
2018-06-11 16:05:35 +0800

    framework search paths

--- fix_bcsymbolmap_1_3_1.sh
+++ fix_bcsymbolmap_1_3_1.sh
@@ -3,7 +3,10 @@
 poddir=$(echo `gem env gemdir`)
 embed_frameworks_script_file_path="${poddir}/gems/cocoapods-1.3.1/lib/cocoapods/generator/embed_frameworks_script.rb"
 target_integrator_path="${poddir}/gems/cocoapods-1.3.1/lib/cocoapods/installer/user_project_integrator/target_integrator.rb"
+xcconfig_helper_path="${poddir}/gems/cocoapods-1.3.1/lib/cocoapods/generator/xcconfig/xcconfig_helper.rb"
 echo "copying embed_frameworks_script.rb to ${embed_frameworks_script_file_path}"
 echo "copying target_integrator.rb to ${target_integrator_path}"
+echo "copying xcconfig_helper.rb to ${xcconfig_helper_path}"
 curl -fsSL https://raw.githubusercontent.com/mingway1991/PodFramework/master/embed_frameworks_script.rb -o "$embed_frameworks_script_file_path"
 curl -fsSL https://raw.githubusercontent.com/mingway1991/PodFramework/master/target_integrator.rb -o "$target_integrator_path"
+curl -fsSL https://raw.githubusercontent.com/mingway1991/PodFramework/master/xcconfig_helper.rb -o "$xcconfig_helper_path"
